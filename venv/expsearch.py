03f86d0237a1d07ec8fe5a2806eb483a61b4e500
bellyache-g <blake.arw@gmail.com>
2021-07-20 22:40:03 -0700

    tried printing the body text on found links but erowid is weird in its structure

--- venv/expsearch.py
+++ venv/expsearch.py
@@ -10,18 +10,20 @@ urlcount = 0
 
 for i in range(900000):
     updatedurl = F'https://www.erowid.org/experiences/exp.php?ID={i}'
-
     urlcount += 1
-    print('This is URL ' + str(urlcount) + ' is something here?')
+    print('trying URL ' + str(urlcount) + ' is something here?')
     urlget = requests.get(updatedurl)
     urltry = urlget.content
     soup = BeautifulSoup(urltry, 'html.parser')
     issomethinghere = soup.find_all("div", class_="title")
     if issomethinghere == []:
-        print('nothing is here')
+        print('no')
     else:
-        print('something is here: ' + str(issomethinghere))
+        print('yes: ' + str(issomethinghere))
         print(updatedurl)
+#       craziness = soup.find('body')
+#       print(craziness)
+
     #time.sleep(.2)
 
 
